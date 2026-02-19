4b491f1da87bfe211f03b3d2e618dc8cade3ccff
Bryan Wadsworth <wadsworth.bryan@gmail.com>
2026-02-19 00:22:33 -0700

    Camera now follows player with smooth lerp

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,13 +1,22 @@
-import { useThree } from '@react-three/fiber'
-import { useEffect } from 'react'
+import { useFrame, useThree } from '@react-three/fiber'
+import { Vector3 } from 'three'
+
+import { usePlayerStore } from '../store/playerStore'
 
 export default function Camera() {
   const { camera } = useThree()
+  const position = usePlayerStore((state) => state.position)
 
-  useEffect(() => {
-    camera.position.set(0, 25, 25)
-    camera.lookAt(0, 0, 0)
-  }, [camera])
+  useFrame(() => {
+    const targetX = position[0]
+    const targetZ = position[2]
+
+    const offset = new Vector3(0, 25, 25)
+    const targetPosition = new Vector3(targetX + offset.x, offset.y, targetZ + offset.z)
+
+    camera.position.lerp(targetPosition, 0.05)
+    camera.lookAt(targetX, 0, targetZ)
+  })
 
   return null
 }
