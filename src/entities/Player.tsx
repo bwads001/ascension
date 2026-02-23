72309ef16711bca398e51e6c4dbfe98bf773cd41
Bryan Wadsworth <wadsworth.bryan@gmail.com>
2026-02-22 23:01:49 -0700

    feat: add player light source for dungeon exploration
    - Player has point light (intensity 5, distance 12, warm white color)
    - Reduce ambient back to 0.6 for dungeon atmosphere

--- src/entities/Player.tsx
+++ src/entities/Player.tsx
@@ -182,6 +182,7 @@ export default function Player({ playerClass = 'warrior' }: PlayerProps) {
         lockRotations
       >
         <CuboidCollider args={[0.4, 1, 0.4]} position={[0, 1, 0]} />
+        <pointLight position={[0, 2, 0]} intensity={5} distance={12} color="#ffe4c4" />
       </RigidBody>
     )
   }
@@ -197,6 +198,7 @@ export default function Player({ playerClass = 'warrior' }: PlayerProps) {
       <group scale={isAttacking ? 1.1 : 1}>
         <Character isHit={isHit} />
       </group>
+      <pointLight position={[0, 2, 0]} intensity={5} distance={12} color="#ffe4c4" />
     </RigidBody>
   )
 }
